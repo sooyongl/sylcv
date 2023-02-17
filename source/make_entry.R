d3862541db7fc44f132cf7af31266693b60752a3
Sooyong Lee <sooyongl09@utexas.edu>
2023-02-17 13:37:01 -0600

    update cv

---
+++ source/make_entry.R
@@ -0,0 +1,109 @@
+mk_entry <- function(data, what=NA, when=NA, with=NA, where=NA, why=NA, more_why = NA) {
+  # data = a1
+  cols <- c("what"=what, "when"=when, "with"=with, "where"=where, "why"=why, "more_why" = more_why)
+  
+  check_name<-function(data, cols) {
+    for(a in cols) {
+      names(data)[which(names(data)==a)] <- names(cols)[which(cols==a)]
+    }
+    data
+  }
+  
+  out <- check_name(data, cols)
+  
+  if(sum(any(names(out)=="what"), na.rm = T) == 0) {
+    out$what <- ""
+  }
+  
+  if(sum(any(names(out)=="when"), na.rm = T) == 0) {
+    out$when <- ""
+  }
+  
+  if(sum(any(names(out)=="with"), na.rm = T) == 0) {
+    out$with <- ""
+  }
+  if(sum(any(names(out)=="where"), na.rm = T) == 0) {
+    out$where <- ""
+  }
+  
+  if(sum(any(names(out)=="why"), na.rm = T) == 0) {
+    
+    why0 <- ""
+    
+  } else {
+    why_part <- str_split(out$why, ";", simplify = F) 
+    why0 <- c(); i <- 0
+    for(wp in why_part){
+      i <- i + 1
+      
+      if(is.na(wp)) {
+        why_itemize <- ""
+      } else {
+        why_itemize <- ""  
+        for(wpp in wp) {
+          why_itemize <- paste0(why_itemize,"\\item ", wpp," ")
+        }
+      }
+      
+      why0[i] <- why_itemize
+    }
+  }
+  
+  if(sum(any(names(out)=="more_why"), na.rm = T) == 0) {
+    
+    more_why0 <- ""
+    
+  } else {
+    morewhy_part <- str_split(out$more_why, ";", simplify = F) 
+    more_why0 <- c(); i <- 0
+    for(wp in morewhy_part){
+      i <- i + 1
+      
+      if(is.na(wp)) {
+        
+        morewhy_itemize <- ""
+        
+      } else {
+        morewhy_itemize <- " \\begin{itemize}"
+        for(wpp in wp) {
+          morewhy_itemize <- paste0(morewhy_itemize,"\\item item ", wpp," ")
+        }
+        morewhy_itemize <- paste0(morewhy_itemize, " \\end{itemize}")
+        
+      }
+      more_why0[i] <- morewhy_itemize
+    }
+  }
+  
+  
+  out <- out %>% 
+    mutate(why = why0) %>% 
+    mutate(more_why = more_why0) %>% 
+    mutate_all(~ as.character(.x)) %>% 
+    mutate_all(~ case_when(is.na(.x) ~ "", 
+                           str_detect(.x, "NA") ~ "", 
+                           str_detect(.x, "NULL") ~ "", 
+                           TRUE ~ .x)) %>% 
+    mutate(
+      tex = paste0("\\cventry{",
+                   what,"}{",
+                   with,"}{",
+                   where,"}{",
+                   when,"}{\\begin{cvitems}",
+                   why, 
+                   
+                   more_why,
+                   
+                   "\\end{cvitems}}")
+    ) %>% 
+    pull(tex)
+  
+  out <- paste(out, collapse = "\n\n")
+  
+  cat(paste(
+    c("\\begin{cventries}",
+      out,
+      "\\end{cventries}"),
+    collapse = "\n"
+  ))
+}
---
+++ source/make_pub.R
@@ -0,0 +1,80 @@
+mk_pub <- function(data, exclude = NULL) {
+  
+  indentation <- 
+    "\\hangindent=2em
+\\hangafter=1"
+  
+  data <- data %>% arrange(year, author) # %>% select(all_of(selection))
+  
+  if(!is.null(exclude)) {
+    
+    for(ex in exclude) {
+      data <- data %>% 
+        mutate(!!as.name(ex) := NA)
+    }
+  }
+  
+  data <- 
+    data %>% 
+    mutate(author = str_replace(author, "Sooyong Lee", "**Sooyong Lee**"),
+           journal = paste0("*",journal,"*."),
+           volum = paste0(" ",volum,","),
+           number = paste0(" ",number,"."),
+           doi = paste0(" DOI:", doi,"."),
+           note = paste0(" ",note,".")
+    ) %>% 
+    mutate_all(~ as.character(.x)) %>% 
+    mutate_all(~ case_when(is.na(.x) ~ "", 
+                           str_detect(.x, "NA") ~ "", 
+                           str_detect(.x, "NULL") ~ "", 
+                           TRUE ~ .x)) %>% 
+    mutate(
+      out = 
+        glue::glue("{indentation}\n{author} ({year}). {title}. {journal}{volum}{number}{doi}{note}")
+    )
+  
+  out <- data %>% pull(out)
+  out <- paste(out, collapse = "\n\n")
+  
+  cat(out)
+}
+
+# Presentation
+mk_presen <- function(data, exclude = NULL) {
+  
+  indentation <- 
+    "\\hangindent=2em
+\\hangafter=1"
+  
+  data <- data %>% arrange(year, author) # %>% select(all_of(selection))
+  
+  if(!is.null(exclude)) {
+    
+    for(ex in exclude) {
+      data <- data %>% 
+        mutate(!!as.name(ex) := NA)
+    }
+  }
+  
+  data <- 
+    data %>% 
+    mutate(author = str_replace(author, "Sooyong Lee", "**Sooyong Lee**"),
+           title = paste0("*",title,"*"),
+           conference = paste0(conference,","),
+           location = paste0(" ",location,".")
+    ) %>% 
+    mutate_all(~ as.character(.x)) %>% 
+    mutate_all(~ case_when(is.na(.x) ~ "", 
+                           str_detect(.x, "NA") ~ "", 
+                           str_detect(.x, "NULL") ~ "", 
+                           TRUE ~ .x)) %>% 
+    mutate(
+      out = 
+        glue::glue("{indentation}\n{author} ({year}). {title}. {conference}{location}")
+    )
+  
+  out <- data %>% pull(out)
+  out <- paste(out, collapse = "\n\n")
+  
+  cat(out)
+}
