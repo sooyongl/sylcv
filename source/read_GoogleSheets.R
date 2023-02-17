62676387de152ca5f73e26c54b54b8d13a4d6203
Sooyong Lee <sooyongl09@utexas.edu>
2023-02-17 11:59:29 -0800

    update cv

--- source/read_GoogleSheets.R
+++ source/read_GoogleSheets.R
@@ -1,6 +1,8 @@
 readGoogleSheets <- function(sheet_name) {
   
-  cvInfo <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/10ye0YoRjDaCrN4nc1YiFXO8DRtVSyTg1B4z_w8icLtI/edit#gid=0", sheet = sheet_name)
+  cvInfo <- readxl::read_excel("data/CV_infolist.xlsx", sheet = sheet_name)
+  
+  # cvInfo <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/10ye0YoRjDaCrN4nc1YiFXO8DRtVSyTg1B4z_w8icLtI/edit#gid=0", sheet = sheet_name)
   
   cvInfo
 }
